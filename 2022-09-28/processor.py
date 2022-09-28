3151549ffe61800b6260a408b678061c993304ee
James Powell <james@dutc.io>
2022-09-28 14:48:24 -0400

    small changes to fit to screen

--- 2022-09-28/processor.py
+++ 2022-09-28/processor.py
@@ -23,33 +23,24 @@ class RawData:
 
     @classmethod
     def from_path(cls, path):
-        if not (mo := cls.STEM_RE.fullmatch(path.stem)) or path.suffix not in cls.PROCESSORS:
-            raise ValueError('invalid input file path')
-        date, details = mo.group('date'), mo.group('details')
-        date = datetime.strptime(date, '%Y-%m-%d')
-        processors = cls.PROCESSORS[path.suffix]
-        return cls(path, path.with_stem(f'processed.{date:%Y-%m-%d}.{details}'), date, details, processors)
+        if (mo := cls.STEM_RE.fullmatch(path.stem)) and path.suffix in cls.PROCESSORS:
+            date, details = datetime.strptime(mo.group('date'), '%Y-%m-%d'), mo.group('details')
+            processors = cls.PROCESSORS[path.suffix]
+            return cls(path, path.with_stem(f'processed.{date:%Y-%m-%d}.{details}'), date, details, processors)
 
 if __name__ == '__main__':
-    all_files = {
-        RawData.from_path(p)
-        for p in Path('/tmp/data').iterdir()
-        if RawData.STEM_RE.fullmatch(p.stem) and p.suffix in {'.csv', '.json'}
-    }
+    all_files = {RawData.from_path(p) for p in Path('data').iterdir()}
+    skipped, corrupt = {Path('results.2022-09-28.test-run.csv')}, {Path('results.2022-09-27.bad-run.csv')}
 
-    skipped = {Path('results.2022-09-28.test-run.csv')}
-    corrupt = {Path('results.2022-09-27.bad-run.csv')}
-
-    to_process = {rd for rd in all_files if rd.input not in skipped | corrupt}
+    to_process = {rd for rd in all_files if rd and rd.input not in skipped | corrupt}
     for ent in to_process:
-        data = (
+        (data :=
             ent.processors.reader(ent.input)
-            .assign(timestamp=lambda df: to_timedelta(df['time']) + ent.date)
-            .drop('time', axis='columns')
-            .set_index(['entity', 'timestamp'])
-            .groupby(['entity', Grouper(level='timestamp', freq='H')]).max()
-            .groupby('entity').transform(
-                lambda g: g.rolling(3, min_periods=1, win_type='triang').mean()
-            )
-            .pipe(ent.processors.writer)(ent.output)
-        )
+                .assign(timestamp=lambda df: to_timedelta(df['time']) + ent.date)
+                .drop('time', axis='columns')
+                .set_index(['entity', 'timestamp'])
+                .groupby(['entity', Grouper(level='timestamp', freq='H')]).max()
+                .groupby('entity').transform(
+                    lambda g: g.rolling(3, min_periods=1, win_type='triang').mean()
+                )
+        ).pipe(ent.processors.writer)(ent.output)
